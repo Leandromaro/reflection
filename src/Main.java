552f249c02d38d2cb6ddd4e1d1889e9d75fa0abe
Leandro Maro <leandro.maro@globant.com>
2018-04-30 18:18:04 -0300

    Refactoring old for each method

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,18 @@
 import java.lang.reflect.Constructor;
-import java.lang.reflect.Method;
-import java.util.Arrays;
+import static java.util.Arrays.stream;
 
 public class Main {
     public static void main(String[] args) {
         for (String name : args) {
+            System.out.println("Methods from the class " + name);
             Class<?> clazz = name.getClass();
+            // I let the constructors just like an example
             Constructor<?>[] constructors = clazz.getConstructors();
-            Method[] methods = clazz.getMethods();
-            for (Method method : methods
-                    ) {
+            stream(clazz.getMethods()).forEach(method -> {
                 System.out.println("Method " + method.getName());
-                Arrays.stream(method.getParameterTypes()).forEach(parameter -> System.out.println("Parameter: " + parameter.getName()));
-            }
+                stream(method.getParameterTypes())
+                        .forEach(parameter -> System.out.println("Parameter: " + parameter.getName()));
+            });
         }
     }
 }
